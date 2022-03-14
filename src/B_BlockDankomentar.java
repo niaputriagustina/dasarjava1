eaaf2ad77db349a376733be73a3589d3061c6a03
PC 10 <niaputriagustina12082000@gmail>
2022-03-14 16:35:02 +0700

    blok dan komentar

--- src/B_BlockDankomentar.java
+++ src/B_BlockDankomentar.java
@@ -1,6 +1,1 @@
-public class B_BlockDankomentar {
-
-    public static  void main(String[] args) {
-        System.out.println(" pemograman beorientasi objek dengsn java");
-    }
-}
+public class B_BlockDankomentar { public static  void main(String[] args) {System.out.println(" pemograman beorientasi objek dengsn java");}}
