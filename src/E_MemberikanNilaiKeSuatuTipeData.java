d8062f1b036e122cee1d67c289506fc1bf98691f
PC 10 <niaputriagustina12082000@gmail>
2022-03-14 17:07:46 +0700

    Memberikan Nilai ke Suatu Tipe Data

---
+++ src/E_MemberikanNilaiKeSuatuTipeData.java
@@ -0,0 +1,20 @@
+public class E_MemberikanNilaiKeSuatuTipeData {
+    public static void main(String[] args) {
+        boolean b=true;
+        System.out.println("value b="+b);
+        char c ='c';
+        System.out.println("value c="+c);
+        byte bt=10;
+        System.out.println("value bt="+ bt);
+        short s=20;
+        System.out.println("value s="+ s);
+        int i=30;
+        System.out.println("vslue i="+i);
+        long l=40L;
+        System.out.println("value l="+l);
+        float f=3.14f;
+        System.out.println("vslue f"+f);
+        double d=3.14;
+        System.out.println("value d="+d);
+    }
+}
