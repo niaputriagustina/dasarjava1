0d8670072518879b304963e64b565d26723e272c
PC 10 <niaputriagustina12082000@gmail>
2022-03-14 17:15:19 +0700

    Mencetak Nilai Konstanta

---
+++ src/G_MencetakNilaiKonstanta.java
@@ -0,0 +1,9 @@
+public class G_MencetakNilaiKonstanta {
+    public static void main(String[] args) {
+        final double PHI=3.14;
+        final double CM_PER_INCH=2.54;
+        System.out.println("Nilai konstanta PHI="+PHI);
+        System.out.println("Nilai konstanta CM_PER_INCH="+CM_PER_INCH);
+    }
+
+}
