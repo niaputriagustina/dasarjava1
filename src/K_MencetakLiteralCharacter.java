5f8d6468be8be3d4f73533177ea1357bb45c3427
PC 10 <niaputriagustina12082000@gmail>
2022-03-19 17:22:59 +0700

    K_MencetakLiteralCharacter

---
+++ src/K_MencetakLiteralCharacter.java
@@ -0,0 +1,13 @@
+public class K_MencetakLiteralCharacter {
+    public static void main(String[] args) {
+        char tab='\t';
+        char newline='\n';
+        char singleQuote='\'';
+        char doubleQuote='\"';
+        System.out.println("ini contoh"+tab+"literal tab");
+        System.out.println("ini contoh"+newline+"literal newline");
+        System.out.println(""+singleQuote+"ini contoh literal single quote"+singleQuote);
+        System.out.println(""+doubleQuote+"ini contoh literal double quote"+doubleQuote);
+
+    }
+}
